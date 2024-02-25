9d4237c4d990839fb7af38682a92c29265a9dbf2
Ciaran De Groot <ciaran.deg@gmail.com>
2024-02-25 12:07:11 -0700

    Add flake-parts

--- flake.nix
+++ flake.nix
@@ -1,10 +1,10 @@
 {
-  inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
-  };
+  inputs.nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
 
-  outputs = {...}: {
-    nixosModules = {
+  inputs.flake-parts.url = "github:hercules-ci/flake-parts";
+
+  outputs = {flake-parts, ...} @ inputs: flake-parts.lib.mkFlake {inherit inputs;} {
+    flake.nixosModules = {
       hardware-linode = import ./nix/hardware/linode;
       hardware-digitalocean-droplet = import ./nix/hardware/digitalocean;
     };
