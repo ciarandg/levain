ba85c584fd976a980309afa6671ac9d11367e1a1
Ciaran De Groot <ciaran.deg@gmail.com>
2024-02-25 12:47:19 -0700

    Use path instead of string

--- nix/users/default.nix
+++ nix/users/default.nix
@@ -1,7 +1,7 @@
 {config, ...}: {
   config = {
     users.users.nixos = {
-      hashedPasswordFile = "/nixos_hashed_password";
+      hashedPasswordFile = /nixos_hashed_password;
       extraGroups = ["wheel" "sudo"];
       isNormalUser = true;
       openssh.authorizedKeys.keyFiles = ["/nixos_authorized_keys"];
