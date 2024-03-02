161fa58919112ea86372bd1835bc530206acfb66
Ciaran De Groot <ciaran.deg@gmail.com>
2024-03-02 15:15:02 -0700

    Add mandatory levain.user.authorizedKeys option

--- nix/user/default.nix
+++ nix/user/default.nix
@@ -1,4 +1,12 @@
-{config, ...}: {
+{config, lib, ...}: let
+  cfg = config.levain.user;
+in {
+  options = {
+    levain.user.authorizedKeys = lib.mkOption {
+      description = "A list of SSH public keys to connect to this instance with";
+      type = lib.types.listOf lib.str;
+    };
+  };
   config = {
     users = {
       mutableUsers = false;
@@ -6,7 +14,7 @@
         hashedPasswordFile = "/nixos_hashed_password";
         extraGroups = ["wheel" "sudo"];
         isNormalUser = true;
-        openssh.authorizedKeys.keyFiles = ["/nixos_authorized_keys"];
+        openssh.authorizedKeys.keys = cfg.authorizedKeys;
       };
     };
   };
