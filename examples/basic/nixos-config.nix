65867713aa6cc60dd75ba7188d8c8a815ae53a8c
Ciaran De Groot <ciaran.deg@gmail.com>
2024-03-04 19:37:47 -0700

    Add note

--- examples/basic/nixos-config.nix
+++ examples/basic/nixos-config.nix
@@ -12,7 +12,8 @@
       hardware.linode.enable = true;
       user.authorizedKeys = [
         # Put the contents of your public key here!
-        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEKPiQGm1ZXWXa6mIwMHqlNaMiSMfK0UjVc8LZ7VhjmM admin@pc"
+        # NOTE: Should be identical to Terraform var.ssh_public_key
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHw1Sn/UDAG1IoXMUiN+kuM9aiSub+t8kE6yUXlp76nY admin@pc"
       ];
     };
 
