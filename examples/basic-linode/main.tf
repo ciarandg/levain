6079ee06cb4111a513239969872ff8ae0ce75078
Ciaran De Groot <ciaran.deg@gmail.com>
2024-03-10 15:17:30 -0600

    Use keyfile data sources instead of variables
    Instead of storing keyfile data in variables, use gitignored files

--- examples/basic-linode/main.tf
+++ examples/basic-linode/main.tf
@@ -3,19 +3,18 @@ variable "linode_token" {
   sensitive = true
 }
 
-variable "ssh_private_key" {
-  type = string
-  sensitive = true
+data "local_file" "ssh_public_key" {
+  filename = "${path.module}/example_ed25519.pub"
 }
 
-variable "ssh_public_key" {
-  type = string
+data "local_sensitive_file" "ssh_private_key" {
+  filename = "${path.module}/example_ed25519"
 }
 
 module "node" {
   source            = "github.com/ciarandg/levain//terraform/platform/node-linode"
   nixos_config_name = "example"
-  ssh_public_key    = var.ssh_public_key
-  ssh_private_key   = var.ssh_private_key
+  ssh_public_key    = trimspace(data.local_file.ssh_public_key.content)
+  ssh_private_key   = data.local_sensitive_file.ssh_private_key.content
   env_suffix        = "-development"
 }
